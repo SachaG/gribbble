36654dbc027a315027f9d8c61e890c8e1cc2076e
Sacha Greif <sacha357@gmail.com>
2014-03-16 11:12:11 +0000

    Fading loading div out after subscription is loaded.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 Session.get('skip', 0);
 
 Deps.autorun(function(){
-  Meteor.subscribe('snapshots', 12, Session.get('skip'));
+  Meteor.subscribe('snapshots', 12, Session.get('skip'), function(){
+    $('.loading').fadeOut('slow');
+  });
 });
